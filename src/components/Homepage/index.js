51588e0270fd44eaa8cb2a1f3291cb4c0c98638b
Anabel <villalobos1998@gmail.com>
2022-06-09 15:59:26 -0700

    finished homepage

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,4 +1,6 @@
 import './Homepage.css'
+import me from '../../assets/images/me.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Homepage = () => {
 
@@ -6,17 +8,25 @@ export const Homepage = () => {
 
     return (
         <div className="homepage-container">
-            <section>
-                <p>
-                My name is Anabel Villalobos and welcome to my personal website. The day I stumbled across CodeAcademy is when I decided I found everythin I was looking for in a career. 
-                I loved the rush of executing coding challenges and lessons. Coding reminded me of video games. Trying to solve a bug is like trying to beat a game. I instantly fell in love. 
-                Shortly after realizing this is what I want to do for the rest of my life, I enrolled in AppAcademy. Here, I coded 
-                for around 60 hours a week studying various languages while collaborating with diverse developers daily. I was able to apply my skills at an unbelievable rate 
-                and my curiosity to learn more increased. At AppAcademy, I learned an extensive range of technical skills. Not only that but I also learned how to master and quickly 
-                apply skills. I am super eager to prove my value and talent at a company that has the same energy. I love working on teams who love to create and keep each other motivated. 
-                I hope to rise in this sort of environment, bringing the engineering skills I continue to develop everyday along with me.
-                </p>
-            </section>
+            <div className="about-me-container">
+                <section id="about-me">
+                    <h1 style={{textAlign:"center"}}>Hello World</h1>
+                    <p>
+                    My name is Anabel Villalobos and welcome to my personal website. I'm a Software Engineer that 
+                    specializes in full-stack web development and am experienced in building & utilizing modern 
+                    frontend and backend development tools like React, Redux, JavaScript, Nodejs. Docker, AWS, 
+                    CSS, HTML, PostgreSQL, Sequelize, SQLAlchemy, Github, Python, & Flask to deliver fast applications. 
+                    I am also experienced in writing tests and testing with Mocha and Chai libraries. I love the challenge,
+                    the thrill and the thought, an imagination can come to life with code. I have developed an insatiable 
+                    appetite for turning lines of code into real life applications. My primary focus currently is becoming
+                    a better Developer on various tech stacks. I am both driven and self-motivated, constantly seeking to
+                    learn new technologies and frameworks
+                    </p>
+                    <a href="https://github.com/annvilla1998"><i className="fa-brands fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/anabel-villalobos-5772ab196/"><i className="fa-brands fa-linkedin-in"></i></a>
+                </section>
+                <img src={me}/>
+            </div>
         </div>
     )
 }
