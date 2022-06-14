3d0ac1aaf0f7552d2cb7fa0fb556fc309fdc0b53
Anabel <villalobos1998@gmail.com>
2022-06-14 13:54:16 -0700

    commented out unused

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Navigation } from './components/Navigation/index'
 import { Homepage } from './components/Homepage/index'
 import { Resume } from './components/Resume/index'
-import { Portfolio } from './components/Portfolio/index'
+// import { Portfolio } from './components/Portfolio/index'
 import { Contact } from './components/Contact/index'
 
 function App() {
